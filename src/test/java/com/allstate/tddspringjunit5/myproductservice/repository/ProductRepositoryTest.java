1330334940d768178d7f15a23f2fee660f543bcb
Brant Collins <lcomy@allstate.com>
2020-05-20 12:43:24 -0500

    adds repository test and potentially useful repository test config

---
+++ src/test/java/com/allstate/tddspringjunit5/myproductservice/repository/ProductRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.allstate.tddspringjunit5.myproductservice.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class ProductRepositoryTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Test
+    void injectedComponentsAreNotNull() {
+        assertThat(dataSource).isNotNull();
+        assertThat(jdbcTemplate).isNotNull();
+        assertThat(entityManager).isNotNull();
+        assertThat(repository).isNotNull();
+    }
+}
